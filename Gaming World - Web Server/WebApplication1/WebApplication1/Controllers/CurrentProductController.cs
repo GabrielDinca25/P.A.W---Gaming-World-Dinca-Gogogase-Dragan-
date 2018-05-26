b853de7ab8d22029f5cfa19551101017ac841eb0
Gabi\Gabriel <gdincagabriel@gmail.com>
2018-05-26 10:59:29 +0300

    Added template page for each product

---
+++ Gaming World - Web Server/WebApplication1/WebApplication1/Controllers/CurrentProductController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class CurrentProductController : Controller
+    {
+        private GameDBContext db;
+
+        public CurrentProductController(GameDBContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        [ActionName("CurrentProduct")]
+        public IActionResult CurrentProduct()
+        {
+            String productName = HttpContext.Request.Query["productName"].ToString();
+
+            var currentProduct = db.Games.Where(g => g.Name.Equals(productName)).FirstOrDefault();
+
+            if(currentProduct != null)
+            {
+                ViewData["currentProduct"] = currentProduct;
+                return View();
+            } 
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+
+        }
+    }
+}
